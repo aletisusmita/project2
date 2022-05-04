d3aac8d21de5ca4894304d03d199ab1574fecfa6
Pankajsingh2110 <ddmukharji@gmail.com>
2022-05-04 22:40:20 +0530

    project 2

---
+++ src/Models/interModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+
+const internSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: {
+        type: String, unique: true, validate: {
+            validator: function (v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/.test(v)
+
+            },
+            message: props => `${props.value} is not a valid email address........`
+        },
+        required: [true, 'user email must be required']
+    },
+
+
+
+    mobile: {
+        type: Number, unique: true,
+        validate: {
+            validator: function (v) {
+                return /^\(?([6-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v)                     ///\d{6-9}-\d{3}-\d{4}/.test(v)
+            },
+            message: props => `${props.value} is not a valid mobile number.....`
+        },
+        required: [true, 'user mobile number must be required']
+    },
+
+
+    collegeId: {
+        type: ObjectId, required: true,
+        ref: "College"
+    },
+    isDeleted: {
+        type: Boolean, default: false
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("Intern", internSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,15 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken")
-
-const authorController = require("../controllers/authorController")
-const blogController = require ("../controllers/blogController")
-const middleware = require("../middlewares/auth")
-//===========================================Phase-1============================================================
-
-router.post("/createAuthor", authorController.createAuthor)
-
-// router.post("/createBlog",  blogController.createBlog)
-// router.get("/getBlogs", blogController.getBlogs)
-// router.put("/updateBlog/:blogId", blogController.updateBlog)
-// router.delete("/deleteBlog/:blogId",blogController.deleteBlog)
-// router.delete("/deleteQuery", blogController.deleteQuery)
 
 
-//================================================Phase-2=========================================================
+const collegecontroller = require("../controllers/collegeController")
 
-router.post("/login",authorController.login)
+const internController = require("../controllers/internController")
 
-router.post("/createBlog", middleware.authenticate, blogController.createBlog)
-router.get("/getBlogs",middleware.authenticate, blogController.getBlogs)
-router.put("/updateBlog/:blogId",middleware.authenticate, middleware.authorise, blogController.updateBlog)
-router.delete("/deleteBlog/:blogId",middleware.authenticate, middleware.authorise, blogController.deleteBlog)
-router.delete("/deleteQuery", middleware.authenticate, middleware.authorise, blogController.deleteQuery)
+router.post("/functionup/colleges", collegecontroller.createCollege)
 
+router.post("/functionup/interns", internController.createIntern)
 
+router.get("/functionup/collegeDetails",internController.getInter)
 
-module.exports=router 
+module.exports = router 
